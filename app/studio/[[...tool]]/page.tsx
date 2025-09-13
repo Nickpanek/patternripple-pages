bda85887dd60ed1ef4bbfb978466e6e8e8a0ad75
Nick Panek <nickpanek.ks@gmail.com>
2025-09-12 20:56:55 -0500

    Update page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -2,9 +2,9 @@
 export const runtime = 'edge';
 export const dynamic = 'force-static';
 
-import dynamic from 'next/dynamic';
+import NextDynamic from 'next/dynamic';
 
-const StudioClient = dynamic(() => import('./StudioClient'), { ssr: false });
+const StudioClient = NextDynamic(() => import('./StudioClient'), { ssr: false });
 
 export default function StudioPage() {
   return <StudioClient />;
