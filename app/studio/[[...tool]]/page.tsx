8d4177135be11af2d3ae3f5077b8d9aee205f753
Nick Panek <nickpanek.ks@gmail.com>
2025-09-12 19:52:26 -0500

    Update page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,9 +1,7 @@
-'use client'
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
-
-export const runtime = 'edge'
+// Server wrapper so Studio can render fresh on each request
+export const dynamic = 'force-dynamic';
+import StudioClient from './StudioClient';
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <StudioClient />;
 }
