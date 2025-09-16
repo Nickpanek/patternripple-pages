aa0ba7ed7266779e4fd3ec91858d5d6439400138
Nick Panek <nickpanek.ks@gmail.com>
2025-09-16 00:39:57 -0500

    Update page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import {NextStudio} from 'next-sanity/studio'
+import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
-export const runtime = 'nodejs' // Studio must run on Node, not Edge
+export const runtime = 'nodejs'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
