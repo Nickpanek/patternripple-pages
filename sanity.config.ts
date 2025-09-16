db1bb5ea42cbe89c7ae14544dab31cc409fe65ec
Nick Panek <nickpanek.ks@gmail.com>
2025-09-15 22:46:20 -0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,15 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'PatternRipple',
-  
   projectId: 'zomio78q',
   dataset: 'production',
-  
-  plugins: [structureTool()],
-  
+  basePath: '/studio', // important for routing
+  plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
