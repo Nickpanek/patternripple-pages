de5f4604b76b75f5d2cd3ea98c3c299cfe58a6d6
Nick Panek <nickpanek.ks@gmail.com>
2025-09-16 00:37:06 -0500

    Update page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,26 +1,9 @@
 'use client'
-export const runtime = 'edge'
+import {NextStudio} from 'next-sanity/studio'
+import config from '../../../sanity.config'
+
+export const runtime = 'nodejs' // Studio must run on Node, not Edge
 
 export default function StudioPage() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>Pattern Studio</h1>
-      <p>Access your Sanity Studio:</p>
-      <a 
-        href="https://www.sanity.io/manage/personal/project/zomio78q" 
-        target="_blank"
-        style={{ 
-          background: '#6366f1', 
-          color: 'white', 
-          padding: '12px 24px', 
-          textDecoration: 'none', 
-          borderRadius: '6px',
-          display: 'inline-block',
-          marginTop: '20px'
-        }}
-      >
-        Open Sanity Studio
-      </a>
-    </div>
-  )
+  return <NextStudio config={config} />
 }
