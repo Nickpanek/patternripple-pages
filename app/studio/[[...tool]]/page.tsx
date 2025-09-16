58b82b71da16451b8d174065dfe3f54475b0e9ba
Nick Panek <nickpanek.ks@gmail.com>
2025-09-15 23:47:58 -0500

    Update page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,18 +1,8 @@
 'use client'
 
-export const runtime = 'edge'
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
-import dynamicImport from 'next/dynamic'
-
-const Studio = dynamicImport(
-  () => import('next-sanity/studio').then(m => m.NextStudio),
-  { ssr: false }
-)
-
+import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
 export default function StudioPage() {
-  return <Studio config={config} />
+  return <NextStudio config={config} />
 }
