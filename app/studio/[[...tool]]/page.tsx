1e5c4248202eec70a8c4c65b54abed73d5992223
Nick Panek <nickpanek.ks@gmail.com>
2025-09-15 23:17:02 -0500

    Update page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -4,13 +4,14 @@ export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
-import dynamic from 'next/dynamic'
+import dynamicImport from 'next/dynamic'
 import config from '../../../sanity.config'
 
-// render Studio only on the client to avoid blank screen
-const Studio = dynamic(() => import('next-sanity/studio').then(m => m.NextStudio), {
-  ssr: false,
-})
+// client-only render to avoid blank screen
+const Studio = dynamicImport(
+  () => import('next-sanity/studio').then(m => m.NextStudio),
+  { ssr: false }
+)
 
 export default function StudioPage() {
   return <Studio config={config} />
