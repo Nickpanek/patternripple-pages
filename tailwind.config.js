206bd94158934613696c70c1ed2cd1e609b9a4eb
Nick Panek <nickpanek.ks@gmail.com>
2025-09-17 04:35:09 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          300: '#FFE55C',
+          400: '#FFD700',
+          500: '#FFC700',
+          600: '#FFB300',
+        }
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
