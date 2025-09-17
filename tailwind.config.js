242d95bc6afa973c1494e9b2235709960d4c1ceb
Nick Panek <nickpanek.ks@gmail.com>
2025-09-17 05:27:58 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        gold: {
-          300: '#FFE55C',
-          400: '#FFD700',
-          500: '#FFC700',
-          600: '#FFB300',
-        }
-      },
-      animation: {
-        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
