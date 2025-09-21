2778fa0f12240ca5e19683e072041cdaa012686f
Nick Panek <nickpanek.ks@gmail.com>
2025-09-21 10:24:34 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'cdn.sanity.io' }] }
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "cdn.sanity.io" },          // existing listings
+      { protocol: "https", hostname: "files.patternripple.com" } // your R2 image host
+    ],
+  },
 };
